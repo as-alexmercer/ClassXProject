851efa62988459efe49c54bd41fff588b8dcd4ac
Alex Mercer <alexmerser@gmail.com>
2020-12-16 23:42:06 +0530

    main

--- 2nd_Term/Program1.java
+++ 2nd_Term/Program1.java
@@ -11,19 +11,26 @@ class Program1
         int m1[] = new int[n];
         int m2[] = new int[n];
         int m3[] = new int[n];
-        System.out.println("Enter the Name, Roll. No., Marks in 3 Subjects");
         for(int i=0;i<n;i++)
         {
+            System.out.println("Enter the Name");
             name[i]=sc.nextLine();
+            System.out.println("Enter the Roll no.");
             roll[i]=sc.nextInt();
+            System.out.println("Enter the Marks 1");
             m1[i]=sc.nextInt();
+            System.out.println("Enter the Marks 2");
             m2[i]=sc.nextInt();
+            System.out.println("Enter the Marks 3");
             m3[i]=sc.nextInt();
         }
         for(int i=0;i<n;i++)
         {
-            double avg = (m1[i]+m2[i]+m3[i])/3;
-            
+            double avg = (m1[i]+m2[i]+m3[i])/3.0;
+            if (avg > 99 && avg <= 85)
+            {
+
+            }
         }
     }
 }
